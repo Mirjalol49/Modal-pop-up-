553add65dd6f18e6c20a6e753de157173a92a585
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-05-21 20:56:09 +0500

    commented

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const modalXBtn = document.querySelector(".modal-x-btn");
 const overlay = document.querySelector(".overlay");
 const modalInput = document.querySelector(".modal-input");
 
-//Open modal
+//!Open modal
 function open() {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
   modalInput.value = "";
 }
 
-//Close modal
+//!Close modal
 function close() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 }
 
+//!Buy button, to open modal
 buyBtn.addEventListener("click", open);
 
+//!X button in modal to close
 modalXBtn.addEventListener("click", close);
 
+//!Overlay in background to close, when it is clicked to body
 overlay.addEventListener("click", close);
