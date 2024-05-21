38d8fa21d5a7a41d24b5b14266d35cca690a84f0
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-05-21 20:36:36 +0500

    ///

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const modal = document.querySelector(".modal");
 const buyBtn = document.querySelector(".content-btn");
 const modalXBtn = document.querySelector(".modal-x-btn");
 const overlay = document.querySelector(".overlay");
+const modalInput = document.querySelector(".modal-input");
 
 //Open modal
 function open() {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
+  modalInput.value = "";
 }
 
 //Close modal
